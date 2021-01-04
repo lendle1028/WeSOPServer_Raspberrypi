91e72c24bcbfd7e89dc2aaab1d62991fce94af9e
lendle <lendle_tseng@seed.net.tw>
2021-01-04 22:09:19 +0800

    add more tester tabs

--- src/main/java/rocks/imsofa/wesop/server/ui/TesterController.java
+++ src/main/java/rocks/imsofa/wesop/server/ui/TesterController.java
@@ -20,6 +20,64 @@ public class TesterController {
 
     @FXML
     private TextField textFileName;
+    
+    @FXML
+    private TextField textTerminalCommand;
+    
+    @FXML
+    private TextField textServerIP;
+
+    @FXML
+    private TextField textKeepAliveURL;
+    
+    @FXML
+    private TextField textMessageTitle;
+
+    @FXML
+    private TextField textMessage;
+    
+    @FXML
+    void onShowMessageGOButtonClicked(ActionEvent event) {
+        final Command command = new Command();
+        command.setGroupName("com.example.lendle.esopserver.commands");
+        command.setName("showMessage");
+        Map params=new HashMap<String, Object>();
+        params.put("title", textMessageTitle.getText());
+        params.put("message", textMessage.getText());
+        command.setParams(params);
+        this.sendCommand(command);
+    }
+    
+    @FXML
+    void onBackHomeGOButtonClicked(ActionEvent event) {
+        final Command command = new Command();
+        command.setGroupName("com.example.lendle.esopserver.commands");
+        command.setName("home");
+        this.sendCommand(command);
+    }
+    
+    @FXML
+    void onSetServerIPGOButtonClicked(ActionEvent event) {
+        final Command command = new Command();
+        command.setGroupName("com.example.lendle.esopserver.commands");
+        command.setName("setServerIP");
+        Map params=new HashMap<String, Object>();
+        params.put("ip", textServerIP.getText());
+        params.put("keepAliveURL", textKeepAliveURL.getText());
+        command.setParams(params);
+        this.sendCommand(command);
+    }
+    
+    @FXML
+    void onTerminalCommandGOButtonClicked(ActionEvent event) {
+        final Command command = new Command();
+        command.setGroupName("com.example.lendle.esopserver.commands");
+        command.setName("command");
+        Map params=new HashMap<String, Object>();
+        params.put("command", textTerminalCommand.getText());
+        command.setParams(params);
+        this.sendCommand(command);
+    }
 
     @FXML
     void onFileCopyGOButtonClicked(ActionEvent event) {
