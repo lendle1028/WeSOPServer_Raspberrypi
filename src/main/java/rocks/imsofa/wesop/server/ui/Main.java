d75080c4b0558dbd48bc86529c66f1a570e6d4c6
lendle <lendle_tseng@seed.net.tw>
2021-01-21 16:15:42 +0800

    bundle javafx components

--- src/main/java/rocks/imsofa/wesop/server/ui/Main.java
+++ src/main/java/rocks/imsofa/wesop/server/ui/Main.java
@@ -5,47 +5,17 @@
  */
 package rocks.imsofa.wesop.server.ui;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
 /**
  *
  * @author lendle
  */
-public class Main extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-            
-            primaryStage.setTitle("WeSOP");
-            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-                @Override
-                public void handle(WindowEvent event) {
-                    System.exit(0);
-                }
-            });
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+public class Main{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Main_FX.main(args);
     }
     
 }
