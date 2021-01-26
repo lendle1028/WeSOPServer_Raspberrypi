a41edfd6a6006b09480b4fb3c53ad97abe94ae3f
lendle <lendle_tseng@seed.net.tw>
2021-01-26 16:50:27 +0800

    create install sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+mkdir ~/.wesop
+copy WeSOPServer-1.0-jar-with-dependencies.jar ~/.wesop/
+copy ./run.sh ~/.wesop/
+sudo copy wesop.desktop /etc/xdg/autostart
+sudo apt-get update
+sudo apt-get install ristretto
