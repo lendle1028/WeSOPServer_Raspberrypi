e8ef6f63fa7559cfbacfa31fcfb2cafab18fb2d4
lendle <lendle_tseng@seed.net.tw>
2021-01-27 16:45:07 +0800

    prepare for deb file

--- src/main/java/rocks/imsofa/wesop/server/CheckNetworkStatusThread.java
+++ src/main/java/rocks/imsofa/wesop/server/CheckNetworkStatusThread.java
@@ -1,13 +1,15 @@
 package rocks.imsofa.wesop.server;
 
 import com.google.gson.Gson;
+import java.io.InputStream;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import org.apache.commons.io.IOUtils;
 import rocks.imsofa.wesop.server.util.IDUtil;
 import rocks.imsofa.wesop.server.util.MulticastUtil;
 
@@ -15,84 +17,123 @@ import rocks.imsofa.wesop.server.util.MulticastUtil;
  * Created by lendle on 2015/6/22.
  */
 public class CheckNetworkStatusThread extends Thread {
-    private boolean running=true;
-    private long interval=100;
+
+    private boolean running = true;
+    private long interval = 100;
     /**
      * if back from a network failed state, recovery is needed
      */
-    private boolean recoveryNeeded=false;
-    public CheckNetworkStatusThread(){
+    private boolean recoveryNeeded = false;
+    private long serverlastSeen=System.currentTimeMillis();
+
+    public CheckNetworkStatusThread() {
     }
 
-    public void run(){
-        while(running){
-            Date startDate = new Date( );
-            try{
+    public void run() {
+        while (running) {
+            try {
                 Thread.sleep(interval);
                 //DebugUtils.log("server ip: "+GlobalContext.serverIP);
-                if(!recoveryNeeded){
-                    if(GlobalContext.serverIP==null){
+                if (!recoveryNeeded) {
+                    if (GlobalContext.serverIP == null) {
+                        long now=System.currentTimeMillis();
+                        if(now-serverlastSeen>5*60*1000){//server not seen for more than 5 minutes
+                            recoveryNeeded=true;
+                            DebugUtils.log(Level.SEVERE, "server not seen......");
+                            continue;
+                        }
                         //set or reset serverIP
-                        String ip=null;
+                        String ip = null;
                         //get intranet ip
-                        Enumeration<NetworkInterface> enumeration=NetworkInterface.getNetworkInterfaces();
-                        outer: while(enumeration.hasMoreElements()){
-                            NetworkInterface networkInterface=enumeration.nextElement();
-                            Enumeration<InetAddress> addresses= networkInterface.getInetAddresses();
-                            while(addresses.hasMoreElements()){
-                                InetAddress address=addresses.nextElement();
+                        Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+                        outer:
+                        while (enumeration.hasMoreElements()) {
+                            NetworkInterface networkInterface = enumeration.nextElement();
+                            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                            while (addresses.hasMoreElements()) {
+                                InetAddress address = addresses.nextElement();
                                 //DebugUtils.log(address.getHostAddress());
-                                if(address.getHostAddress().startsWith("192") || address.getHostAddress().startsWith("10.0")){
-                                    ip=address.getHostAddress();
+                                if (address.getHostAddress().startsWith("192") || address.getHostAddress().startsWith("10.0")) {
+                                    ip = address.getHostAddress();
                                     break outer;
                                 }
                             }
                         }
-                        GlobalContext.IP=ip;
-                        Map<String, String> data=new HashMap<String, String>();
-                        String id= IDUtil.getId();
+                        GlobalContext.IP = ip;
+                        Map<String, String> data = new HashMap<String, String>();
+                        String id = IDUtil.getId();
                         data.put("id", id);
                         data.put("ip", GlobalContext.IP);
-                        String dataString=new Gson().toJson(data);
+                        String dataString = new Gson().toJson(data);
                         MulticastUtil.multicast("224.0.0.100", 5003, dataString);
+                    }else{
+                        serverlastSeen=System.currentTimeMillis();
                     }
-                }else{
+                } else {
                     //TODO: re-implement connectivity check
                     //reboot servers, reset central server ip
                     //check network connectivity at first
-                    if (GlobalContext.serverIP != null){
+                    boolean connection2ServerisOk = true;
+                    if (GlobalContext.serverIP != null && GlobalContext.keepAliveURL != null) {
+                        try {
+                            InputStream finishInput = GlobalContext.keepAliveURL.openStream();
+                            IOUtils.readLines(finishInput, "utf-8");
+                            finishInput.close();
+                            serverlastSeen=System.currentTimeMillis();
+                        } catch (Exception e) {
+                            //DebugUtils.log("fail to issue keepAlive Packet");
+                            DebugUtils.log(e, false);
+                            connection2ServerisOk = false;
+                        }
+                    }
+                    GlobalContext.server.stopServers();
+                    Thread.sleep(1000);
+                    GlobalContext.server.startServers();
+                    if (!connection2ServerisOk) {
+                        //restart servers
+                        //reset server ip
+                        GlobalContext.serverIP = null;
+                        
+                    }
+                    recoveryNeeded = false;
+                    /*ConnectivityManager cm =
+                            (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    if(cm==null){
+                        DebugUtils.log("cm=null");
+                        Thread.sleep(1000);
+                        continue;
+                    }
+                    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                    if(activeNetwork==null){
+                        DebugUtils.log("activeNetwork=null");
+                        Thread.sleep(1000);
+                        continue;
+                    }
+                    if(activeNetwork!=null && activeNetwork.isConnected()) {
                         DebugUtils.log("recovery for "+ IDUtil.getId()+"......");
                         GlobalContext.serverIP = null;
                         GlobalContext.server.stopServers();
                         Thread.sleep(1000);
                         GlobalContext.server.startServers();
                         recoveryNeeded=false;
-                    }
-                    else{
-                        long startTime = startDate.getTime();
-                        long currentTime = new Date().getTime();
-                        long diff = currentTime - startTime;
-                        if((diff/(1000 * 60)) > 1){
-                            DebugUtils.log("network problem for "+ IDUtil.getId()+"......");
-                        }
-                        Thread.sleep(1000);
+                    }else {
                         continue;
-                    }
+                    }*/
                 }
 
-                interval=5000;
+                interval = 5000;
                 //check network state
-                if(!GlobalContext.server.isServerAllOn()){
-                    recoveryNeeded=true;
+                if (!GlobalContext.server.isServerAllOn()) {
+                    recoveryNeeded = true;
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 DebugUtils.log(e.toString());
             }
         }
     }
 
-    public void shutdown(){
-        this.running=false;
+    public void shutdown() {
+        this.running = false;
         this.interrupt();
     }
 }
